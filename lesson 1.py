2051f85c6c9e4ab1d19daf1af9dd4ab2ec267679
andreyka101 <andrejkvasnin236@gmail.com>
2023-06-12 16:27:03 +0300

    уауау

--- lesson 1.py
+++ lesson 1.py
@@ -1,18 +1,19 @@
-file = open('tyy.txt')
-fileData = ''
-for i in file:
-    fileData += i
-print(fileData)
-
-
-
 # file = open('tyy.txt')
-# fileData = 'ththth'
-# clippedData = ''
-# s = 5
+# fileData = ''
 # for i in file:
 #     fileData += i
-# while (s != len(fileData)):
-#     clippedData += fileData[s]
-#     s+=1
 # print(fileData)
+
+
+
+file = open('tyy.txt')
+fileData = ''
+clippedData = ''
+s = 5
+for i in file:
+    fileData += i
+
+while (s != len(fileData)):
+    clippedData += fileData[s]
+    s+=1
+print(clippedData)  
