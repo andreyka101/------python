47f81fbce65ad07c4e425566c20924d79fd47ec0
andreyka101 <andrejkvasnin236@gmail.com>
2023-06-12 21:16:59 +0300

    dgdgg

--- exam number 17.py
+++ exam number 17.py
@@ -49,14 +49,15 @@
 
 
 
-count = 0
-m = 0
-print ("ffgff")
-f = open('17.txt')
-l = [int(i) for i in f]
-for i in range(len(l) - 1):
-    for j in range(i + 1, len(l)):
-        if (l[i] + l[j]) %2 != 0 and (l[i] * l[j]) % 3 == 0:
-            count += 1
-            m = max(m, l[i] + l[j])
-print(count, m)
+# count = 0
+# m = 0
+# print ("ffgff")
+# f = open('17.txt')
+# l = [int(i) for i in f]
+# print (l)
+# for i in range(len(l) - 1):
+#     for j in range(i + 1, len(l)):
+#         if (l[i] + l[j]) %2 != 0 and (l[i] * l[j]) % 3 == 0:
+#             count += 1
+#             m = max(m, l[i] + l[j])
+# print(count, m)
