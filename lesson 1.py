c57b73b175760daef1f9b4f3fcf6955fddb6b18b
andreyka101 <andrejkvasnin236@gmail.com>
2023-06-12 14:18:00 +0300

    first commit

---
+++ lesson 1.py
@@ -0,0 +1,18 @@
+file = open('tyy.txt')
+fileData = ''
+for i in file:
+    fileData += i
+print(fileData)
+
+
+
+# file = open('tyy.txt')
+# fileData = 'ththth'
+# clippedData = ''
+# s = 5
+# for i in file:
+#     fileData += i
+# while (s != len(fileData)):
+#     clippedData += fileData[s]
+#     s+=1
+# print(fileData)
