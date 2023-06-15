8d7763e1c4455dbddd38fde4f51d8beb7e985717
andreyka101 <andrejkvasnin236@gmail.com>
2023-06-15 10:26:44 +0300

    rhrgry

---
+++ exam number 15.py
@@ -0,0 +1,18 @@
+# for a in range(1000):
+#     t = 0
+#     for x in range(1000):
+#         if ((x & 29 != 0) <= ((x & 17 == 0) <= (x & a != 0))):
+#             t+=1
+#     if (t == 1000):
+#         print(a)
+#         break
+
+
+# p=range(4, 16)
+# q=range(12,20)
+# for A in range(100):
+#     for x in range(100):
+#         if (((x in p) and ((x in q))) <= (x in int(A))):
+#             A=A[1:]
+#         else:
+#             print(len(A))
