9c73989d171dee0e82e9182b0f9f42ced853bc4f
andreyka101 <andrejkvasnin236@gmail.com>
2023-06-17 10:20:43 +0300

    rfgrgrg

--- exam number 15.py
+++ exam number 15.py
@@ -9,14 +9,36 @@
 
 
 
-p=range(4, 16)
-q=range(12,20)
-A=[int(i) for i in range(1,16)]
-for x in range(100):
-    if (((x in p) and ((x in q))) <= (x in A)):
-        A=A[1:]
-    else:
-        print(len(A))
+# p=range(4, 16)
+# q=range(12,20)
+# A=[int(i) for i in range(1,16)]
+# for x in range(100):
+#     if (((x in p) and ((x in q))) <= (x in A)):
+#         A=A[1:]
+#     else:
+#         print(len(A))
 
 
 # print ("5" in "5")
+
+
+
+# for a in range(1000):
+#     t = 0
+#     for x in range(1000):
+#         if ((x & 29 != 0) <= ((x & 17 == 0) <= (x & a != 0))):
+#             t+=1
+#     if (t == 1000):
+#         print(a)
+#         break
+
+
+
+# for a in range():
+#     k = 0
+#     for x in range(1, 1000):
+#         if (x%a!=0)<=((x%6==0)<=(x%4!=0)):
+#             k += 1
+#     if k == 999:
+#         print(a)
+#         break
